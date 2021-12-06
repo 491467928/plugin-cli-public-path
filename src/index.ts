4e6454eabe2ae1754ba0f418ccb9c96ab12120f8
wanxl <wanxl@idste.cn>
2021-12-06 15:13:11 +0800

    初始化项目

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {IPluginContext} from '@tarojs/service'
+
+export default (ctx: IPluginContext, pluginOpts) => {
+    ctx.onBuildStart(() => {
+        if (ctx.runOpts.options.platform === 'h5') {
+            const pathIndex = process.argv.findIndex(p => p === '--publicPath');
+            if (pathIndex < process.argv.length - 1) {
+                const publicPath = process.argv[pathIndex + 1];
+                console.log('modify publicPath to:', publicPath);
+                if (ctx.initialConfig.h5) {
+                    ctx.modifyWebpackChain(({chain}) => {
+                        chain.output.publicPath(publicPath);
+                    });
+                }
+            }
+        }
+    })
+}
