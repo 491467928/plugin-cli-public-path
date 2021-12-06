dcc5d655668a816d1a5b77b76f02fa717283c82d
wanxl <wanxl@idste.cn>
2021-12-06 15:58:21 +0800

    修复参数判断bug

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export default (ctx: IPluginContext, pluginOpts) => {
     ctx.onBuildStart(() => {
         if (ctx.runOpts.options.platform === 'h5') {
             const pathIndex = process.argv.findIndex(p => p === '--publicPath');
-            if (pathIndex < process.argv.length - 1) {
+            if (pathIndex > -1 && pathIndex < process.argv.length - 1) {
                 const publicPath = process.argv[pathIndex + 1];
                 console.log('modify publicPath to:', publicPath);
                 if (ctx.initialConfig.h5) {
